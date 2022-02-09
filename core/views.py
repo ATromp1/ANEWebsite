54e707303e11cf5603dad76295fa7ea85ad5a04e
bergstromkasper <64561347+Malpiyt@users.noreply.github.com>
2022-02-09 23:09:15 +0100

    Benched roster per boss in event-view

--- core/views.py
+++ core/views.py
@@ -136,18 +136,18 @@ def add_user_to_roster_button(request, event_date):
 
 def get_playable_classes_as_css_classes():
     playable_classes = {
-        'warrior': 'Warrior',
-        'paladin': 'Paladin',
-        'hunter': 'Hunter',
-        'rogue': 'Rogue',
-        'priest': 'Priest',
-        'shaman': 'Shaman',
-        'mage': 'Mage',
-        'warlock': 'Warlock',
-        'monk': 'Monk',
-        'druid': 'Druid',
-        'demonhunter': 'Demon Hunter',
-        'deathknight': 'Death Knight',
+        'Warrior': 'warrior',
+        'Paladin': 'paladin',
+        'Hunter': 'hunter',
+        'Rogue': 'rogue',
+        'Priest': 'priest',
+        'Shaman': 'shaman',
+        'Mage': 'mage',
+        'Warlock': 'warlock',
+        'Monk': 'monk',
+        'Druid': 'druid',
+        'Demon Hunter': 'demonhunter',
+        'Death Knight': 'deathknight',
     }
     return playable_classes
 
--- static/js/event_view.js
+++ static/js/event_view.js
@@ -5,6 +5,7 @@ for (i = 0; i < boss_list.length; i++) {
         'id': i
     })
 }
+ 
 
 let roles_per_class = {
     'Warrior': ['tank', 'mdps'],
@@ -20,7 +21,7 @@ let roles_per_class = {
     'Demon Hunter': ['tank', 'mdps'],
     'Death Knight': ['tank', 'mdps']
 }
-
+let class_roles =['tank', 'healer','mdps','rdps']
 
 let roster_characters = []
 for (let i in roster) {
@@ -35,10 +36,17 @@ for (let i in roster) {
 
 class RaidEvent {
     constructor(bosses, initial_roster) {
+        // Bosses in a raid
         this.bosses = bosses;
+
+        // Initial roster is the roster we get from DB
         this.initial_roster = initial_roster;
 
+        // instances of class RosterPerBoss in an array to access them
         this.roster_per_boss_objects = []
+
+        // This should be a boss_id
+        this.currently_selected_boss_roster = -1
     }
 
 
@@ -52,8 +60,14 @@ class RaidEvent {
     }
 
     switch_to_roster(boss_id){
-        console.log("switch_to_roster bossid: " + boss_id)
-        roster_per_boss_objects[boss_id].update_roster_display()
+        /*
+        Switches to a different roster(boss) in the info view. Changes 'currently_selected_boss_roster' to the new boss
+        */
+        if(boss_id != this.currently_selected_boss_roster){
+            console.log("switch_to_roster bossid: " + boss_id)
+            this.currently_selected_boss_roster = boss_id
+            this.roster_per_boss_objects[boss_id].update_roster_display()
+        }
     }
 
 
@@ -66,47 +80,66 @@ class RosterPerBoss {
     constructor(boss, initial_roster) {
         this.boss = boss;
         this.initial_roster = initial_roster
-        this.roster = []
+        this.benched_roster = initial_roster
+        this.selected_roster = []
     }
 
-    populate_roster() {
-        for (let character in this.initial_roster) {
-            this.roster[character] = new RosterCharacter(this.initial_roster.name,
-                this.initial_roster.playable_class,
-                this.initial_roster.role)
-        }
-    }
 
-    update_roster_display(){
-        $('.event-view-boss-info').empty();
-        HTMLtoAppend = ''
-        let roster = raid_event.roster_per_boss_objects[this.boss];
-        HTMLtoAppend = HTMLtoAppend + roster.boss
-        for(let char in roster.initial_roster){
-            HTMLtoAppend = HTMLtoAppend +'<div class="'+ css_classes[roster.initial_roster[char].playable_class] +'">' + roster.initial_roster[char].name;
 
-            for(let role in roster.initial_roster[char].roles){
-                HTMLtoAppend = HTMLtoAppend + '<div class="event-view-role-icon" id="'+ roster.initial_roster[char].roles[role] +' '+ roster.initial_roster[char].name+'"></div>';
+    update_roster_display(roster){
+        /*
+        This function doesn't change any variables, simply updates the display to this RosterPerBoss class
+        It only updates the display
+        */
+
+        $('.event-view-available-roster-table').empty();
+        // First clear all old buttons
+        $('.boss-view-btn').removeClass('active');
+
+        // Highlights the button of the currently selected boss
+        $('.boss-view-btn#'+this.boss).addClass('active')
+
+        let images_path = 'images/roleIcons/'
+        for(let char in this.benched_roster){
+            let char_name = this.benched_roster[char].name
+            let char_css_class = css_classes[this.benched_roster[char].playable_class]
+
+            let event_view_table_row =  '<tr>'+'<td class="'+char_css_class+'">'+char_name+'</td>'
+
+            if(this.benched_roster[char].roles.includes('tank')){
+                event_view_table_row = event_view_table_row + '<td><img src="'+static_url+images_path+'tank.png" alt="Tank"></td>'
+            }else{
+                event_view_table_row = event_view_table_row + '<td></td>'
             }
 
-            HTMLtoAppend = HTMLtoAppend + '</div>';
+            if(this.benched_roster[char].roles.includes('healer')){
+                event_view_table_row = event_view_table_row + '<td><img src="'+static_url+images_path+'healer.png" alt="Healer"></td>'
+            }else{
+                event_view_table_row = event_view_table_row + '<td></td>'
+            }
 
+            if(this.benched_roster[char].roles.includes('mdps')){
+                event_view_table_row = event_view_table_row + '<td><img src="'+static_url+images_path+'mdps.png" alt="Melee dps"></td>'
+            }else{
+                event_view_table_row = event_view_table_row + '<td></td>'
+            }
+
+            if(this.benched_roster[char].roles.includes('rdps')){
+                event_view_table_row = event_view_table_row + '<td><img src="'+static_url+images_path+'rdps.png" alt="Ranged dps"></td>'
+            }else{
+                event_view_table_row = event_view_table_row + '<td></td>'
+            }
+
+            event_view_table_row = event_view_table_row + '</tr>';
+            $('.event-view-available-roster-table').append(event_view_table_row);
         }
-        $('.event-view-boss-info').append(HTMLtoAppend);   
     }
 }
 
-class RosterCharacter {
-    constructor(name, playable_class, role) {
-        this.boss_name = boss_name;
-        this.name = name;
-        this.playable_class = playable_class;
-        this.role = role;
-    }
-}
 
 raid_event = new RaidEvent(boss_name_list, roster_characters)
 raid_event.populate_boss_rosters()
+
 
 //Create boss buttons
 let HTMLtoAppend = ''
@@ -116,7 +149,7 @@ for(i = 0; i < boss_name_list.length; i++){
 $('.event-view-boss-list').append(HTMLtoAppend);
 
 // If element with class '.boss-view-btn' gets clicked then get element id and call
-// update_roster_display() with boss id same as button
+// RaidEvent.switch_to_roster() with boss id same as button
 $('.boss-view-btn').click(function(){
-    raid_event.switch_to_roster(this.id)  // roster_per_boss_objects[this.id].update_roster_display()
+    raid_event.switch_to_roster(this.id)
 })
