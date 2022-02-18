237623a1e56e19d4bfbbbefe94ab7fe6ed553dd7
Flint101 <32782666+Flint101@users.noreply.github.com>
2022-02-18 14:34:50 +0100

    scheduler

---
+++ core/scheduler.py
@@ -0,0 +1,7 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+#
+# def start():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(CREATE_NEW_ROSTER_EVENT, 'interval', minutes=10)
+#     scheduler.start()
