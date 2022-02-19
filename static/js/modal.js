7c106d07db82f9b9100281a57360fb8e80023e7f
bergstromkasper <64561347+Malpiyt@users.noreply.github.com>
2022-02-19 20:54:50 +0100

    More information when reporting late on an event

--- static/js/modal.js
+++ static/js/modal.js
@@ -19,7 +19,7 @@ let events_modal_open_btn = $('.events_modal_open_btn')
 let events_modal_close_btn = $('.close-btn')
 let events_modal_submit_btn = $('.submit-btn')
 let events_modal_delete_btn = $('.delete-btn')
-let events_modal_header_date = $('#events-modal-header-date')
+let events_modal_header_date = $('#input-modal-header-date')
 let event_date;
 
 function toggle_events_modal(){
@@ -39,14 +39,14 @@ $(events_modal_close_btn).click(function(){
 $(events_modal_delete_btn).click(function(){
     send_late_ajax(event_date, 0, delete_current='True')
     toggle_events_modal()
-    status_alert(2000, "Late Time Deleted", "warning")
+    status_alert(2000, "Late Time Removed: " + event_date, "warning")
 })
 
 $(events_modal_submit_btn).click(function(){
     let selected_value = $('#events-modal-mins-late-dropdown :selected').val()
     send_late_ajax(event_date, selected_value, delete_current='False')
     toggle_events_modal()
-    status_alert(2000, "Late Time Saved", "success")
+    status_alert(2000, "Late Time Saved: " +event_date +" " +selected_value, "success")
 })
 
 
