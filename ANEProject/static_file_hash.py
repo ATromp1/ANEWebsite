8f70d5baa3e84a5ed7c3cbcd610245ee2b5ace72
bergstromkasper <64561347+Malpiyt@users.noreply.github.com>
2022-05-04 23:08:19 +0200

    Update file hash to read a file instead of .env

--- ANEProject/static_file_hash.py
+++ ANEProject/static_file_hash.py
@@ -1,7 +1,12 @@
-import os
+from pathlib import Path
+
 
 def static_file_hash_processor(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
     ## Adds the static_hash variable to context dict on every view.
     ## It is current commit hash as a query on the end of the filename.
     ## This is to avoid caching errors when pushing CSS changes.
-    return {'static_hash': os.environ.get('static_hash') or ""}
+    with open(str(BASE_DIR) + '/git_commands/git_hash', 'r') as gh:
+        static_hash = gh.readlines()[0]
+    gh.close()
+    return {'static_hash': static_hash or ""}
